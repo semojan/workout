5c35e9e6fac5e83b74fc16d494e08b821e1cf760
semojan <97410424+semojan@users.noreply.github.com>
2024-12-04 18:21:00 +0330

    initialized postgresql connection

---
+++ Infrastructure/database/db.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+});
+
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const app = require("./Presentation/app");
+const dotenv = require("dotenv");
+const pool = require("./Infrastructure/database/db");
+
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 
-app.listen(port);
+pool.connect().then(() => {
+        app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Failed to connect to the database:", err);
+    });
