f1702307beef9cbad305d27593f85c20976facb9
semojan <97410424+semojan@users.noreply.github.com>
2024-12-25 20:51:02 +0330

    added swagger document info for adding workout

--- Presentation/routes/ExerciseRoutes.js
+++ Presentation/routes/ExerciseRoutes.js
@@ -79,6 +79,85 @@ const controller = require("../controllers/Exercise.controller");
 
 router.get("/", controller.GetAllExercises);
 
+
+/**
+ * @swagger
+ * /exercises/addToWorkout:
+ *   post:
+ *     summary: Add an exercise to a workout.
+ *     description: Adds an exercise to a specified workout with details such as duration and order.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               workoutId:
+ *                 type: integer
+ *                 description: The ID of the workout to which the exercise is to be added.
+ *                 example: 8
+ *               exerciseId:
+ *                 type: integer
+ *                 description: The ID of the exercise to be added.
+ *                 example: 25
+ *               duration:
+ *                 type: integer
+ *                 description: The duration of the exercise in minutes.
+ *                 example: 15
+ *               order:
+ *                 type: integer
+ *                 description: The order in which the exercise appears in the workout.
+ *                 example: 4
+ *     responses:
+ *       200:
+ *         description: Exercise added to workout successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Exercise added to workout successfully"
+ *       404:
+ *         description: Workout or exercise not found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "workout not found"
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     status:
+ *                       type: integer
+ *                       example: 404
+ *                     message:
+ *                       type: string
+ *                       example: "the workout to add exercise does not exist"
+ *       500:
+ *         description: Adding exercise failed due to server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "adding exercise failed"
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "Internal server error"
+ */
+
+
 router.post("/addToWorkout", controller.AddToWorkout);
 
 module.exports = router;
--- Presentation/routes/WorkoutPrivateRoutes.js
+++ Presentation/routes/WorkoutPrivateRoutes.js
@@ -2,6 +2,64 @@ const express = require("express");
 const controller = require("../controllers/WorkoutPrivate.controller");
 const router = express.Router();
 
+/**
+ * @swagger
+ * /workout:
+ *   post:
+ *     summary: Create a new workout.
+ *     description: Adds a new workout to the database with details such as name, description, and visibility.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The name of the workout.
+ *                 example: "test workout"
+ *               description:
+ *                 type: string
+ *                 description: A short description of the workout.
+ *                 example: "hello workout"
+ *               public:
+ *                 type: boolean
+ *                 description: Indicates whether the workout is public or private.
+ *                 example: true
+ *     responses:
+ *       200:
+ *         description: Workout created successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "new workout was added successfully."
+ *                 workoutId:
+ *                   type: integer
+ *                   description: The ID of the newly created workout.
+ *                   example: 12
+ *       500:
+ *         description: Failed to create workout due to server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "failed to create workout"
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "Internal server error"
+ */
+
 router.post("/", controller.AddWorkout);
 
 router.put("/:id", controller.UpdateWorkout);
